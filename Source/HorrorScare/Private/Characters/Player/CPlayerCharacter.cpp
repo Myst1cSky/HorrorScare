2020dade3da304e3f2a732ff443b7bcd92151756
Jose Almendarez <skyfan1yt@gmail.com>
2026-09-08 16:12:31 -0500

    Finally added Movement to Player Character and it works | 9/8/26

---
+++ Source/HorrorScare/Private/Characters/Player/CPlayerCharacter.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Player/CPlayerCharacter.h"
+#include "Characters/Player/CPlayerController.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+ACPlayerCharacter::ACPlayerCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(720.f);
+}
+
+void ACPlayerCharacter::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+	UEnhancedInputLocalPlayerSubsystem* EnhancedInputLocalPlayerSubsystem = 
+		GetController<ACPlayerController>()->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+	
+	if (EnhancedInputLocalPlayerSubsystem)
+	{
+		EnhancedInputLocalPlayerSubsystem->ClearAllMappings();
+		EnhancedInputLocalPlayerSubsystem->AddMappingContext(GameplayMappingContext, 0);
+	}
+}
+
+void ACPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpInputAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Jump);
+		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::HandleLookInput);
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::HandleMoveInput);
+	}
+}
+
+void ACPlayerCharacter::HandleLookInput(const struct FInputActionValue& InputActionValue)
+{
+	FVector2D InputAction = InputActionValue.Get<FVector2D>();
+	AddControllerYawInput(InputAction.X);
+	AddControllerPitchInput(InputAction.Y);
+}
+
+void ACPlayerCharacter::HandleMoveInput(const struct FInputActionValue& InputActionValue)
+{
+	FVector2D InputAction = InputActionValue.Get<FVector2D>();
+	InputAction.Normalize();
+	
+	AddMovementInput(GetMoveFwdDirection() * InputAction.Y + GetRightDirection() * InputAction.X);
+}
+
+FVector ACPlayerCharacter::GetRightDirection() const
+{
+	return GetActorRightVector();
+}
+
+FVector ACPlayerCharacter::GetLookFwdDirection() const
+{
+	return GetActorForwardVector();
+}
+
+FVector ACPlayerCharacter::GetMoveFwdDirection() const
+{
+	return FVector::CrossProduct(GetRightDirection(), FVector::UpVector);
+}
---
+++ Source/HorrorScare/Private/Characters/Player/CPlayerCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CCharacter.h"
+#include "CPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACPlayerCharacter : public ACCharacter
+{
+	GENERATED_BODY()
+public:
+	ACPlayerCharacter();
+	virtual void PawnClientRestart() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	//-----------------------------------------------------//
+	//                     Input                          //
+	//----------------------------------------------------//
+private:
+	void HandleLookInput(const struct  FInputActionValue& InputActionValue);
+	void HandleMoveInput(const struct  FInputActionValue& InputActionValue);
+	
+	FVector GetRightDirection() const;
+	FVector GetLookFwdDirection() const;
+	FVector GetMoveFwdDirection() const;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* GameplayMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* JumpInputAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* LookInputAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* MoveInputAction;
+};
